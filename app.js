06db0b64d0cef56c0ecde85c466126b291afb6ab
Jerry Huang <jerry.huang@livebricks.com>
2014-10-13 03:07:08 +0800

    day21

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
+
+// Set DB
+var db_uri = "mongodb://192.168.33.10:27017/TODOs";
+mongoose.connect(db_uri);
 
 // Set routers
 var index = require('./routes/index');
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var express = require('express');
+var TODO = require('../models/todos');
 var router = express.Router();
 
 // Get todo list
@@ -10,9 +11,27 @@ router.get('/:user_id/todos', function(req, res) {
 
 // Create a todo task
 router.post('/:user_id/todos', function(req, res) {
+    var user_id = req.params.user_id;
     var data = req.body;
 
-    res.status(200).json( {success: data } );
+    // insert to db
+    var todo = new TODO();
+    todo.user_id = user_id;
+    todo.content = data.content;
+    todo.created_at = Date.now();
+    todo.updated_at = Date.now();
+
+    todo.save(function (err) {
+        if (err) {
+            res.status(400).json(
+                { error: "insert db error" }
+            );
+        } else {
+            res.status(201).json(
+                todo
+            );
+        }
+    });
 });
 
 // Get a todo task
