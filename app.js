c6108e095f936bed86a72b552802fa2d439f446e
Jerry Huang <jerry.huang@livebricks.com>
2014-10-13 04:07:57 +0800

    Day25

--- app.js
+++ app.js
@@ -17,4 +17,17 @@ app.use( bodyParser.urlencoded({ extended: true }) );
 app.use('/', index);
 app.use('/user', users);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    res.status(404).jsonp({error: "Not Found"});
+    next();
+});
+
+// catch 500
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500).json({error: err.message});
+});
+
 module.exports = app;
